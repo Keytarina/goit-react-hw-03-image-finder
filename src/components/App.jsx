55be7ea03f03806d9f59f2ebcf2e2043288934cd
Kateryna Yakubych <kateryna.yakubych@gmail.com>
2023-08-14 15:52:21 +0300

    Add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+
+import css from 'components/App.module.css';
+
+import { Searchbar } from 'components/Searchbar/Searchbar';
+import { ImageGallery } from 'components/ImageGallery/ImageGallery';
+import { Button } from 'components/Button/Button';
+
+export class App extends Component {
+
+  render () {
+    return (
+      <div className={css.App}>
+        tctgcjtc
+        <Searchbar/>
+        <ImageGallery/>
+        <Button/>
+      </div>
+    );
+  }
 };
